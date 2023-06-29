cd8f59fee3f2ac78b8c09d85c9d60a13f15c8a46
idoLotan <91139751+idoLotan@users.noreply.github.com>
2023-06-29 09:07:31 +0300

    QR update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -125,8 +125,8 @@ function Home() {
               </div>
             </div>
           </div>
-          <div className="w-1/2 max-md:w-full  "></div>
-          <div className="relative bottom-8 hidden  flex  justify-end sm:right-0">
+         
+          <div className="absolute bottom-1 right-5 hidden md:block  ">
             <div className="flex flex-col">
               <img src={QR} className="w-[64px]"></img>
               <div className="text-[#555FD9]">whatsApp</div>
